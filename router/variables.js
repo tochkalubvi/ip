a6cfd162e50390225b55f9a37efb06d4e088099e
Точка Любви <github.tochkalubvi@gmail.com>
2025-01-10 11:36:39 +0300

    Update variables.js

--- router/variables.js
+++ router/variables.js
@@ -1,1 +1,7 @@
 console.log(configure)
+// ПОЛУЧЕНИЕ СОДЕРЖИМОГО СТРАНИЦЫ
+async function get_page(url) {
+  return fetch(url)
+      .then(data => {return data.text()})
+      .catch(error => console.error('Error fetching data:', error));
+}
