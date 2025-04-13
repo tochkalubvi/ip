d098328e769c588638abe329558226e9817fbe83
Точка Любви <github.tochkalubvi@gmail.com>
2025-04-13 17:08:03 +0300

    Delete router/variables.js

--- router/variables.js
+++
@@ -1,7 +0,0 @@
-console.log(configure)
-// ПОЛУЧЕНИЕ СОДЕРЖИМОГО СТРАНИЦЫ
-async function get_page(url) {
-  return fetch(url)
-      .then(data => {return data.text()})
-      .catch(error => console.error('Error fetching data:', error));
-}
